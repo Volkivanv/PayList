6d2a564252d6cec5d5087898a2d226b95f8763d0
Ivan Volkov <volkivanv@mail.ru>
2021-09-14 23:58:45 +0300

    Second commit

--- main.cpp
+++ main.cpp
@@ -5,33 +5,36 @@
 int main() {
     std::ifstream listPay;
     listPay.open("ListPay.txt");
-    std::string maxName;
-    std::string maxFamily;
-    long long maxSum = 0;
-    std::string maxDate;
-    long long total = 0;
+    if(listPay) {
+        std::string maxName;
+        std::string maxFamily;
+        long long maxSum = 0;
+        std::string maxDate;
+        long long total = 0;
 
-    while (!listPay.eof()){
-        std::string name;
-        std::string family;
-        long long sum;
-        std::string date;
+        while (!listPay.eof()) {
+            std::string name;
+            std::string family;
+            long long sum;
+            std::string date;
 
-        listPay >> name;
-        listPay >> family;
-        listPay >> sum;
-        listPay >> date;
-        total+=sum;
-        if(maxSum < sum){
-            maxName = name;
-            maxFamily = family;
-            maxSum = sum;
-            maxDate = date;
+            listPay >> name;
+            listPay >> family;
+            listPay >> sum;
+            listPay >> date;
+            total += sum;
+            if (maxSum < sum) {
+                maxName = name;
+                maxFamily = family;
+                maxSum = sum;
+                maxDate = date;
+            }
         }
+        std::cout << "Total sum is " << total << std::endl;
+        std::cout << "Maximum payment: " << maxName << " " << maxFamily << " " << maxSum << " " << maxDate << std::endl;
+    } else {
+        std::cerr << "File is not found!" <<std::endl;
     }
-    std::cout << "Total sum is " << total << std::endl;
-    std::cout << "Maximum payment: " << maxName << " " << maxFamily << " " << maxSum <<" "<< maxDate << std::endl;
-
 
 
     return 0;
